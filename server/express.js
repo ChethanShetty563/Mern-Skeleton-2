97510b772f70384e47ea80dd4b794cfb54602604
ChethanShetty563 <chethan563@gmail.com>
2022-08-28 00:39:16 +0530

    Configured client

--- server/express.js
+++ server/express.js
@@ -1,15 +1,17 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import compress from 'compression'
-import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
 import helmet from 'helmet';
-import template from '../template'
+import template from '../template';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
-
+import devBundle from './devBundle';
+import path from 'path';
 
 const app = express();
+devBundle.compile(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,18 +19,20 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+const CURRENT_WORKING_DIR = process.cwd();
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
 app.use((err, req, res, next) => {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).json({"error" : err.name + ": " + err.message})
-    }else if (err) {
-      res.status(400).json({"error" : err.name + ": " + err.message})
-      console.log(err)
-    }
-  })
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({ error: err.name + ': ' + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ': ' + err.message });
+    console.log(err);
+  }
+});
 
 // app.get('/', (req,res) => {
 //     res.status(200).send(template);
